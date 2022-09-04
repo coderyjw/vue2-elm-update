12be03f11de2308e411f868e1dce83e29a6c3ab4
叶继伟 <729721503@qq.com>
2022-09-04 23:58:13 +0800

    vue-router和vuex语法升级

--- src/main.js
+++ src/main.js
@@ -1,29 +1,24 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import routes from './router/router'
-import store from './store/'
-import {routerMode} from './config/env'
-import './config/rem'
+import { createApp } from "vue";
+import App from "./App.vue";
+import store from "./store/";
+import "./config/rem";
+import router from "./router/router";
 
-Vue.use(VueRouter)
-const router = new VueRouter({
-	routes,
-	mode: routerMode,
-	strict: process.env.NODE_ENV !== 'production',
-	scrollBehavior (to, from, savedPosition) {
-	    if (savedPosition) {
-		    return savedPosition
-		} else {
-			if (from.meta.keepAlive) {
-				from.meta.savedPosition = document.body.scrollTop;
-			}
-		    return { x: 0, y: to.meta.savedPosition || 0 }
-		}
-	}
-})
+// Vue.use(VueRouter)
+// const router = new VueRouter({
+// 	routes,
+// 	mode: routerMode,
+// 	strict: process.env.NODE_ENV !== 'production',
+// 	scrollBehavior (to, from, savedPosition) {
+// 	    if (savedPosition) {
+// 		    return savedPosition
+// 		} else {
+// 			if (from.meta.keepAlive) {
+// 				from.meta.savedPosition = document.body.scrollTop;
+// 			}
+// 		    return { x: 0, y: to.meta.savedPosition || 0 }
+// 		}
+// 	}
+// })
 
-new Vue({
-	router,
-	store,
-}).$mount('#app')
-
+const app = createApp(App).use(store).use(router).mount("#app");
